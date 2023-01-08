18209e1ee08ec720fd01b1ffd66aff61ef6835e3
egs125 <egs125@gmail.com>
2023-01-08 16:44:22 +0900

    add components & pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  darkMode: "media", // class
+  plugins: [require("@tailwindcss/forms")],
+};
